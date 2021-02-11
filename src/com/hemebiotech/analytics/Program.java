7d79abfe7ba603a8e02a0d9cfc30b39fc4670e89
Sandrine Chastaing <sandrine.chastaing@outlook.fr>
2021-02-11 17:30:27 +0100

    Creation class Symptoms (creation object Symptoms with constructor, get,set) & class Main, deleted classe AnalyticsCounter

--- src/com/hemebiotech/analytics/Program.java
+++ src/com/hemebiotech/analytics/Program.java
@@ -1,9 +1,22 @@
 package com.hemebiotech.analytics;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// first get input
+		 BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		String line = reader.readLine();
+		
+		// next generate output
+		FileWriter writer = new FileWriter ("result.out");
+				writer.write("headache: " + headacheCount + "\n");
+				writer.write("rash: " + rashCount + "\n");
+				writer.write("dialated pupils: " + pupilCount + "\n");
+				writer.close();
 
 	}
 
