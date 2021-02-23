e083378bb87a13fbb57428751b6a62500655ffe3
Sandrine Chastaing <sandrine.chastaing@outlook.fr>
2021-02-23 20:56:51 +0100

    Correction to use key value

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,5 +1,7 @@
 package com.hemebiotech.analytics;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 
 public class AnalyticsCounter {
@@ -8,9 +10,9 @@ public class AnalyticsCounter {
 	private static int headacheCount = 0;	//nombre de symptome : mal de tête
 	private static int rashCount = 0;		// nombre de symptome : eruption
 	private static int pupilCount = 0;	    // nombre de symptome : pupille 
-	
-	
-	
+
+
+
 	private ReadSymptomDataFromFile readSymptomDataFromFile; // read Symptoms from file 
 
 
@@ -24,26 +26,65 @@ public class AnalyticsCounter {
 	/**
 	 * Counter allows you to browse the list and add 1 each time you find the desired symptom
 	 */
-	
+
 	public void Counter () {
 
 		List<String> symptoms = readSymptomDataFromFile.GetSymptoms();
+		HashMap<String,Integer> numberBySymptom = new HashMap<>();
 		for (String symptom : symptoms)
 		{	
 			if (symptom.equals("headache")) {
+				boolean isHeadacheExist=numberBySymptom.containsKey("headache");
+				if (isHeadacheExist) {
+					Integer headacheCount= numberBySymptom.get("headache");
+					headacheCount ++;
+
+					numberBySymptom.replace("headache", headacheCount);
+				}
+				else { 
+					numberBySymptom.put ("headache", 0);
+				}
+
 				headacheCount++;
 				System.out.println("number of headaches :" + headacheCount);
 			}
 			else if (symptom.equals("rash")) {
+				boolean isRashExist = numberBySymptom.containsKey("rash");
+				if (isRashExist) {
+					Integer rashCount = numberBySymptom.get("rash");
+					rashCount ++;
+					numberBySymptom.replace("rash", rashCount);
+				}
+				else {
+					numberBySymptom.put ("rash", 0);	
+				}
+
+
 				rashCount++;
 				System.out.println("number of rash :" + rashCount);
 			}
 			else if (symptom.equals ("pupils")) {
+				boolean isPupilExist = numberBySymptom.containsKey("pupil");
+				if (isPupilExist) {
+					Integer pupilCount = numberBySymptom.get("pupil");
+					pupilCount ++;
+					numberBySymptom.replace("pupil", pupilCount);
+				}
+				else {
+					numberBySymptom.put ("pupil", 0);
+				}
+
 				pupilCount++;
 				System.out.println("number of pupil :" + pupilCount);
 			}
 		}
+		
+		for (Entry<String, Integer> entry : numberBySymptom.entrySet())
+		{
+			System.out.println("For symptom "+ entry.getKey()+ " number of " + entry.getValue());	
+		}
 	}
+
 }
 
 
